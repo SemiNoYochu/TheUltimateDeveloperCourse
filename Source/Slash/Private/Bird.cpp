fb4d4ecb52d39555ee844419806d7732cc8e09ff
Semi <bpwvdf7g55@privaterelay.appleid.com>
2026-05-21 15:17:11 +0900

    [Chapter7] 78~79. Clear
    There are no changes in the C++

--- Source/Slash/Private/Bird.cpp
+++ Source/Slash/Private/Bird.cpp
@@ -1,6 +1,5 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Bird.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
