f4b5b48ccb03f5a96f5cc865469ac03afff2642d
Semi <bpwvdf7g55@privaterelay.appleid.com>
2026-05-22 11:06:40 +0900

    [Chapter8] 86. Hair and Eyebrows

--- Source/Slash/Private/SlashCharacter.cpp
+++ Source/Slash/Private/SlashCharacter.cpp
@@ -5,6 +5,7 @@
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "GroomComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -28,6 +29,14 @@ ASlashCharacter::ASlashCharacter()
 	
 	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
 	ViewCamera->SetupAttachment(SpringArm);
+	
+	Hair = CreateDefaultSubobject<UGroomComponent>(TEXT("Hair"));
+	Hair->SetupAttachment(GetMesh());
+	Hair->AttachmentName = FString("head");
+	
+	Eyebrow = CreateDefaultSubobject<UGroomComponent>(TEXT("Eyebrow"));
+	Eyebrow->SetupAttachment(GetMesh());
+	Eyebrow->AttachmentName = FString("head");
 }
 
 void ASlashCharacter::BeginPlay()
