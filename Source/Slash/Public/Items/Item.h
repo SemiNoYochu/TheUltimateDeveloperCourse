b2c70fff9125bd58606b6fd551ea81034706f8e9
Semi <bpwvdf7g55@privaterelay.appleid.com>
2026-05-27 21:15:05 +0900

    [Chpater10] 101~103. The Weapon Class, Sockets. Downloading Animations

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -44,6 +44,22 @@ protected:
 	
 	template<typename T>
 	T Avg(T First, T Second);
+	
+	UFUNCTION()
+	virtual void OnSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	virtual void OnSphereEndOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex);
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
@@ -53,22 +69,6 @@ private:
 	
 	UPROPERTY(VisibleAnywhere)
 	USphereComponent* Sphere;
-	
-	UFUNCTION()
-	void OnSphereBeginOverlap(
-		UPrimitiveComponent* OverlappedComponent, 
-		AActor* OtherActor, 
-		UPrimitiveComponent* OtherComp, 
-		int32 OtherBodyIndex, 
-		bool bFromSweep, 
-		const FHitResult& SweepResult);
-	
-	UFUNCTION()
-	void OnSphereEndOverlap(
-		UPrimitiveComponent* OverlappedComponent, 
-		AActor* OtherActor, 
-		UPrimitiveComponent* OtherComp, 
-		int32 OtherBodyIndex);
 };
 
 template <typename T>
---
+++ Source/Slash/Public/Items/Weapons/Weapon.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class SLASH_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeapon();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	virtual void OnSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult) override;
+	
+	virtual void OnSphereEndOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex) override;
+	
+};
